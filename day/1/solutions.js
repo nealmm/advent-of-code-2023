7b951c3bec4c3fc44ee71851b11ab654b9dbdc33
Matt Neal <neal.matt.m@gmail.com>
2023-11-30 23:55:06 -0500

    Setup day one

---
+++ day/1/solutions.js
@@ -0,0 +1,13 @@
+const fs = require('node:fs');
+
+// const input = fs.readFileSync('day/1/input.txt', 'utf-8');
+
+function solution1(input) {
+    return 0;
+}
+
+function solution2(input) {
+    return 0;
+}
+
+module.exports = { solution1, solution2 };
---
+++ day/1/solutions.test.js
@@ -0,0 +1,12 @@
+const { solution1, solution2 } = require('./solutions');
+
+const example =
+``;
+
+// test('--- Part One ---', () => {
+//     expect(solution1(example)).toBe(_);
+// });
+
+// test('--- Part Two ---', () => {
+//     expect(solution2(example)).toBe(_);
+// });
