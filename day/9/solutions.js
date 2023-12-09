4db9755a36845527b059bedde6359caeb69c820b
Matt Neal <neal.matt.m@gmail.com>
2023-12-09 13:40:46 -0500

    --- Day 9: Mirage Maintenance ---

--- day/9/solutions.js
+++ day/9/solutions.js
@@ -43,31 +43,29 @@ function part2(input) {
 
     const allZeros = nums => nums.map(n => n == 0).reduce((a, b) => a && b);
 
-    const data = input.split('\n').map(x => x.split(' ').map(y => parseInt(y)));
+    for (line of input.split('\n')) {
+        const sequences = [line.split(' ').map(x => parseInt(x))];
 
-    for (let i = 0; i < data.length; i++) {
-        const sequences = [data[i]];
+        let i = 0;
 
-        let j = 0;
-
-        while (!allZeros(sequences[j])) {
+        while (!allZeros(sequences[i])) {
             const next = [];
 
-            for (let k = 0; k < sequences[j].length - 1; k++) {
-                next.push(sequences[j][k + 1] - sequences[j][k]);
+            for (let j = 0; j < sequences[i].length - 1; j++) {
+                next.push(sequences[i][j + 1] - sequences[i][j]);
             }
 
             sequences.push(next);
-            j++;
+            i++;
         }
 
         for (let k = sequences.length - 2; k >= 0; k--) {
-            const seq = sequences[k];
+            const current = sequences[k];
             const below = sequences[k + 1];
 
-            const extra = seq[0] - below[0];
+            const extra = current[0] - below[0];
 
-            seq.splice(0, 0, extra);
+            current.splice(0, 0, extra);
 
             if (k == 0) {
                 sum += extra;
