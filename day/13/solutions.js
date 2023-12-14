aa7c28a226e23a45147ddae0e6f667150d12ad48
Matt Neal <neal.matt.m@gmail.com>
2023-12-14 06:31:33 -0500

    Refactor day 13 part 2 solution

--- day/13/solutions.js
+++ day/13/solutions.js
@@ -66,178 +66,58 @@ function part1(input) {
 function part2(input) {
     let sum = 0;
 
-    let patternCount = 0;
     for (pattern of input.split('\n\n')) {
-        patternCount++;
         const tiles = pattern.split('\n').map(line => line.split(''));
 
-        const flipTile = (row, col) => {
-            switch (tiles[row][col]) {
-                case '.':
-                    tiles[row][col] = '#';
-                    break;
-
-                case '#':
-                    tiles[row][col] = '.';
-                    break;
-            }
-        };
-
         const rowsReflectAt = (index) => {
-            for (let i = 0; (0 <= index - i) && (index + i + 1 < tiles.length); i++) {
-                const rowAbove = tiles[index - i].join('');
-                const rowBelow = tiles[index + i + 1].join('');
+            let difference = 0;
 
-                if (rowAbove !== rowBelow) {
-                    return false;
+            for (let i = 0; (0 <= index - i) && (index + i + 1 < tiles.length); i++) {
+                const rowAbove = tiles[index - i];
+                const rowBelow = tiles[index + i + 1];
+
+                for (let j = 0; j < tiles[0].length; j++) {
+                    if (rowAbove[j] !== rowBelow[j]) {
+                        difference++;
+                    }
                 }
             }
 
-            return true;
+            return difference;
         };
 
         const colsReflectAt = (index) => {
-            for (let i = 0; (0 <= index - i) && (index + i + 1 < tiles[0].length); i++) {
-                const colLeft = tiles.map(row => row[index - i]).join('');
-                const colRight = tiles.map(row => row[index + i + 1]).join('');
+            let difference = 0;
 
-                if (colLeft !== colRight) {
-                    return false;
+            for (let i = 0; (0 <= index - i) && (index + i + 1 < tiles[0].length); i++) {
+                const colLeft = tiles.map(row => row[index - i]);
+                const colRight = tiles.map(row => row[index + i + 1]);
+
+                for (let j = 0; j < tiles.length; j++) {
+                    if (colLeft[j] !== colRight[j]) {
+                        difference++;
+                    }
                 }
             }
 
-            return true;
+            return difference;
         };
 
         let horizLineOfRefl = undefined;
         let vertLineOfRefl = undefined;
 
         for (let i = 0; i < tiles.length - 1; i++) {
-            if (rowsReflectAt(i)) {
+            if (rowsReflectAt(i) == 1) {
                 horizLineOfRefl = i;
                 break;
             }
         }
 
         for (let i = 0; i < tiles[0].length - 1; i++) {
-            if (colsReflectAt(i)) {
+            if (colsReflectAt(i) == 1) {
                 vertLineOfRefl = i;
                 break;
             }
-        }
-
-        // Now look for possible smudges
-
-        let possibleSmudges = [];
-
-        for (let i = 0; i < tiles.length - 1; i++) {
-            const diffs = [];
-
-            for (let j = 0; (0 <= i - j) && (i + j + 1 < tiles.length); j++) {
-                const rowAbove = tiles[i - j];
-                const rowBelow = tiles[i + j + 1];
-
-                for (let k = 0; k < tiles[0].length; k++) {
-                    if (rowAbove[k] !== rowBelow[k]) {
-                        diffs.push({ row: i - j, col: k });
-                        diffs.push({ row: i + j + 1, col: k });
-                    }
-                }
-            }
-
-            if (diffs.length == 2) {
-                possibleSmudges = diffs;
-            }
-        }
-
-        for (let i = 0; i < tiles[0].length - 1; i++) {
-            const diffs = [];
-
-            for (let j = 0; (0 <= i - j) && (i + j + 1 < tiles[0].length); j++) {
-                const colLeft = tiles.map(row => row[i - j]);
-                const colRight = tiles.map(row => row[i + j + 1]);
-
-                for (let k = 0; k < tiles.length; k++) {
-                    if (colLeft[k] !== colRight[k]) {
-                        diffs.push({ row: k, col: i - j });
-                        diffs.push({ row: k, col: i + j + 1 });
-                    }
-                }
-            }
-
-            if (diffs.length == 2) {
-                possibleSmudges = diffs;
-            }
-        }
-
-        let foundSmudge = false;
-
-        for (point of possibleSmudges) {
-            // console.log(`Testing tile (${point.row}, ${point.col})...`);
-
-            flipTile(point.row, point.col);
-
-            let newHorizLineOfRefl = undefined;
-
-            for (let i = 0; i < tiles.length - 1; i++) {
-                if (rowsReflectAt(i)) {
-                    newHorizLineOfRefl = i;
-
-                    if (i != horizLineOfRefl) {
-                        break;
-                    }
-                }
-            }
-
-            let newVertLineOfRefl = undefined;
-
-            for (let i = 0; i < tiles[0].length - 1; i++) {
-                if (colsReflectAt(i)) {
-                    newVertLineOfRefl = i;
-
-                    if (i != vertLineOfRefl) {
-                        break;
-                    }
-                }
-            }
-
-            // console.log(`\tOld horiz. line of refl. = ${horizLineOfRefl}`);
-            // console.log(`\tNew horiz. line of refl. = ${newHorizLineOfRefl}`);
-            // console.log(`\tOld vert. line of refl. = ${vertLineOfRefl}`);
-            // console.log(`\tNew vert. line of refl. = ${newVertLineOfRefl}`);
-
-            if (newHorizLineOfRefl != horizLineOfRefl || newVertLineOfRefl != vertLineOfRefl) {
-                foundSmudge = true;
-                // console.log(`Smudge found at tile (${point.row}, ${point.col})`)
-
-                if (newHorizLineOfRefl != horizLineOfRefl) {
-                    horizLineOfRefl = newHorizLineOfRefl;
-                }
-                else {
-                    horizLineOfRefl = undefined;
-                }
-
-                if (newVertLineOfRefl != vertLineOfRefl) {
-                    vertLineOfRefl = newVertLineOfRefl;
-                }
-                else {
-                    vertLineOfRefl = undefined;
-                }
-
-                break;
-            }
-            else {
-                flipTile(point.row, point.col);
-            }
-        }
-
-        // if (!foundSmudge) {
-        //     console.log(patternCount);
-        // }
-        if (patternCount == 14) {
-            console.log(horizLineOfRefl);
-            console.log(vertLineOfRefl);
-            console.log(possibleSmudges);
         }
 
         if (vertLineOfRefl != undefined) {
